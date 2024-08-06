6a363bf13b25929c3b3ab296ff921f5237d5d3d2
Guhhsle <guhhsle@proton.me>
2024-08-06 13:47:22 +0200

    Account layer

--- lib/settings/account.dart
+++ lib/settings/account.dart
@@ -9,95 +9,98 @@ import '../template/prefs.dart';
 
 String mail = '', password = '';
 Stream<Layer> accountSet(dynamic d) async* {
-  yield* Database.stream.map((_) {
-    if (Database.logged) {
-      return Layer(
-        action: Setting(
-          Database.user.email ?? 'ERROR',
+  yield accountLayer(0);
+  yield* Database.stream.map(accountLayer);
+}
+
+Layer accountLayer(dynamic non) {
+  if (Database.logged) {
+    return Layer(
+      action: Setting(
+        Database.user.email ?? 'ERROR',
+        Icons.mail_outline_rounded,
+        '',
+        (c) async => Database.user.verifyBeforeUpdateEmail(
+          await getInput(Database.user.email),
+        ),
+      ),
+      list: [
+        Setting(
+          'Encryption key',
+          Icons.key_rounded,
+          '',
+          (c) async {
+            Navigator.of(c).pop();
+            await getKey(pf['encryptKey']);
+          },
+        ),
+        Setting(
+          'Sync timeout',
+          Icons.cloud_sync_rounded,
+          pf['syncTimeout'],
+          (c) => setPref('syncTimeout', (pf['syncTimeout'] + 2) % 20),
+        ),
+        Setting(
+          'Change password',
+          Icons.password_rounded,
+          '',
+          (c) => Database.resetPassword(Database.user.email!, c),
+        ),
+        Setting(
+          'Log out',
+          Icons.person_remove_rounded,
+          '',
+          (c) async {
+            await FirebaseFirestore.instance.enableNetwork();
+            await FirebaseAuth.instance.signInAnonymously();
+            await FirebaseFirestore.instance.disableNetwork();
+          },
+        ),
+      ],
+    );
+  } else {
+    return Layer(
+      action: Setting(
+        'Continue',
+        Icons.keyboard_return_rounded,
+        '',
+        (c) => Database.signIn(
+          mail.trim(),
+          password,
+          c,
+        ),
+      ),
+      list: [
+        Setting(
+          mail == '' ? 'Email' : mail,
           Icons.mail_outline_rounded,
           '',
-          (c) async => Database.user.verifyBeforeUpdateEmail(
-            await getInput(Database.user.email),
-          ),
+          (c) async {
+            mail = await getInput(mail, hintText: 'Email');
+            refreshLayer();
+          },
         ),
-        list: [
-          Setting(
-            'Encryption key',
-            Icons.key_rounded,
-            '',
-            (c) async {
-              Navigator.of(c).pop();
-              await getKey(pf['encryptKey']);
-            },
-          ),
-          Setting(
-            'Sync timeout',
-            Icons.cloud_sync_rounded,
-            pf['syncTimeout'],
-            (c) => setPref('syncTimeout', (pf['syncTimeout'] + 2) % 20),
-          ),
-          Setting(
-            'Change password',
-            Icons.password_rounded,
-            '',
-            (c) => Database.resetPassword(Database.user.email!, c),
-          ),
-          Setting(
-            'Log out',
-            Icons.person_remove_rounded,
-            '',
-            (c) async {
-              await FirebaseFirestore.instance.enableNetwork();
-              await FirebaseAuth.instance.signInAnonymously();
-              await FirebaseFirestore.instance.disableNetwork();
-            },
-          ),
-        ],
-      );
-    } else {
-      return Layer(
-        action: Setting(
-          'Continue',
-          Icons.keyboard_return_rounded,
+        Setting(
+          password == '' ? 'Password' : password,
+          Icons.password_rounded,
           '',
-          (c) => Database.signIn(
-            mail.trim(),
-            password,
-            c,
-          ),
+          (c) async {
+            password = await getInput(password, hintText: 'Password');
+            refreshLayer();
+          },
         ),
-        list: [
-          Setting(
-            mail == '' ? 'Email' : mail,
-            Icons.mail_outline_rounded,
-            '',
-            (c) async {
-              mail = await getInput(mail, hintText: 'Email');
-              refreshLayer();
-            },
-          ),
-          Setting(
-            password == '' ? 'Password' : password,
-            Icons.password_rounded,
-            '',
-            (c) async {
-              password = await getInput(password, hintText: 'Password');
-              refreshLayer();
-            },
-          ),
-          Setting(
-            'Encryption key',
-            Icons.key_rounded,
-            '',
-            (c) async {
-              await getKey(pf['encryptKey']);
-              refreshLayer();
-            },
-          ),
-        ],
-      );
-    }
-  });
+        Setting(
+          'Encryption key',
+          Icons.key_rounded,
+          '',
+          (c) async {
+            await getKey(pf['encryptKey']);
+            refreshLayer();
+          },
+        ),
+      ],
+    );
+  }
 }
 
 Future<void> getKey(String init) async {
